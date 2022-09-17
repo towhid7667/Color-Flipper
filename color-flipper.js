1d16c0fca444a06564ed5ccb15581d60a926682f
towhid7667 <towhidulhq@gmail.com>
2022-09-17 18:49:56 +0600

    ending commit

--- color-flipper.js
+++ color-flipper.js
@@ -1,10 +1,25 @@
-const loadColor = async (Color) => {
-    const res = await fetch('https://www.thecolorapi.com/id?hex=24B1E0');
-    const data = await res.json();
-    displayColor(data);
+const btn = document.getElementById('click-change');
+const colorvalues = document.getElementById('color-code');
+
+
+const HEX = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+btn.addEventListener('click', function () {
+    let hexThing = "#";
+    for (let i = 0; i < 6; i++) {
+        hexThing += HEX[getCode()];
+    }
+
+    // console.log(hexThing);
+    colorvalues.innerText = hexThing;
+    const BG = document.getElementById('BG-HERE');
+    BG.style.backgroundColor = hexThing;
+
+
+});
+
+const getCode = () => {
+    return Math.floor(Math.random() * HEX.length);
 }
 
-const displayColor = (colors) => {
-    console.log(colors);
 
-}
